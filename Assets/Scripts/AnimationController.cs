410a923dafc59851c357c998c2177b5edf9950e5
chemonky <luricardorod@gmail.com>
2016-01-30 23:16:20 -0600

    se agrego animacion del muneco y que pueda llevar un opbjeto de un lado  a otro

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationController : MonoBehaviour {
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float ranim = Input.GetAxis ("Horizontal");
+		if (Input.GetAxis ("Vertical") > 0 || (Input.GetAxis ("Vertical") < 0) || ranim < 0 || ranim > 0) {
+			anim.SetBool ("Move", true);
+		} else {
+			anim.SetBool ("Move", false);
+		}
+
+		anim.SetFloat ("Rotate", Input.GetAxis("Horizontal"));
+
+		if (Input.GetKeyDown (KeyCode.JoystickButton0)) {
+			StartCoroutine (TakeTime());
+		}
+
+
+
+
+	}
+	IEnumerator TakeTime(){
+		anim.SetBool ("Take", true);
+		yield return new WaitForSeconds(2);
+		anim.SetBool ("Take", false);
+	}
+}
---
+++ Assets/Scripts/Take.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(AudioSource))]
+public class Take : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform spawner;
+	private GameObject objMano;
+	public int x,y,z;
+	private bool flag = false;
+	void Start () {
+		//AudioSource audi = GetComponent<AudioSource> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Ray rayo = Camera.main.ScreenPointToRay (new Vector3(x,y,z));
+		Debug.DrawRay(rayo.origin, rayo.direction * 2, Color.yellow);
+		if(Input.GetKeyDown(KeyCode.X) || Input.GetKeyUp(KeyCode.JoystickButton0)){
+			Debug.Log("hoiol");
+			if (Physics.Raycast (rayo, out hit, 2)) {
+				if (hit.collider.tag == "Object") {
+					objMano = hit.collider.gameObject;
+					objMano.gameObject.SetActive(false);
+					Debug.Log ("Object encontrado");
+					flag = true;
+				}
+			}
+		}
+
+		if (flag) {
+			if(Input.GetKeyUp(KeyCode.X) || Input.GetKeyUp(KeyCode.JoystickButton0)){
+				objMano.gameObject.transform.position = spawner.gameObject.transform.position;
+				objMano.gameObject.SetActive(true);
+
+				Debug.Log("hoiol");
+				flag = false;
+			}
+		}
+
+
+	}
+
+
+}
